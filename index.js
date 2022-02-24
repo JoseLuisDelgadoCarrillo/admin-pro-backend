c774c7133e3044c8fef2087ce58272553beb3022
Jose Luis Delgado Carrillo <joseluisdc48@gmail.com>
2022-02-24 12:38:15 -0700

    correcciones

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use('/api/login', require('./routes/auth') );
 app.use('/api/upload', require('./routes/uploads') );
 
 //Lo ultimo
-app.get('*', (res,req) => {
-    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+app.get('*', (req,res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
 });
 
 
